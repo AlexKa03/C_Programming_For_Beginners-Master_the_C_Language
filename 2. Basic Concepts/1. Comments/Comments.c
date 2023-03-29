f073b3695b8175e1d80e9a194da73812249f377c
Alex Kadiyski <alex_kadiyski2003@abv.bg>
2023-03-29 23:00:04 +0300

    Day 1 of this Udemy course
    Day 1 of this Udemy course

---
+++ 2. Basic Concepts/1. Comments/Comments.c
@@ -0,0 +1,14 @@
+/*
+ * Author:  Alexandar Kadiyski
+ * Purpose: This program prints my name on the screen
+ * Date: 29.03.2023
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hi, my name is Alex."); // This line is displaying my name
+
+    return 0;
+}
